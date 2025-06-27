560ea911fd94185b8671f859bd14e15d3f1a1bb1
electric-otter made Unxus! <electric-otter-github@proton.me>
2025-06-27 19:59:20 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import webpack from 'https://cdn.jsdelivr.net/npm/webpack@5.99.9/+esm'
+import browserify from 'https://cdn.jsdelivr.net/npm/browserify@17.0.1/+esm'
+import jsDos from 'https://cdn.jsdelivr.net/npm/js-dos@8.3.20/+esm'
+import verdaccio from 'https://cdn.jsdelivr.net/npm/verdaccio@6.1.4/+esm'
+// we can emulate the FS
+function fs() {
+  const bunlde = <Uint8Array>;
+  const ci = await emulators.dosboxWorker(bundle);
+  
+  // saving
+  const changesBundle = await ci.persist();
+  
+  // <new session>
+  
+  // loading
+  const ci = await emulators.dosboxWorker([bundle, changesBundle]
+  Dos(/*element*/, {
+    url: /* bundle url */,
+    onEvent: ("ci-ready", ci: CommandInterface) {
+        // now ci s ready
+    },
+);
+}
